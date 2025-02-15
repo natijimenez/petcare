2c4fece0d42d60f5df73a9945383c73520da5ae7
natijimenez <nati.jimenezdev@gmail.com>
2025-02-15 16:24:36 -0300

    correccion redireccionamiento

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,35 @@
-import express from "express"
-import cors from "cors"
-import "dotenv/config";
-import { fileURLToPath } from "url"
-import path from "path"
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
-import authRoute from "./src/routes/auth.route.js"
-import checkoutRoute from "./src/routes/checkout.route.js"
-import productoRoute from "./src/routes/producto.route.js"
+import authRoute from './src/routes/auth.route.js'
+import checkoutRoute from './src/routes/checkout.route.js'
+import productoRoute from './src/routes/producto.route.js'
 
 const app = express()
 
 app.use(express.json())
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 )
 
-app.use("/auth", authRoute)
-app.use("/productos", productoRoute)
-app.use("/checkouts", checkoutRoute)
+app.use('/auth', authRoute)
+app.use('/productos', productoRoute)
+app.use('/checkouts', checkoutRoute)
 
-app.use(express.static(path.join(__dirname, "build")))
+app.use(express.static(path.join(__dirname, 'build'), { fallthrough: true }))
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 
 const PORT = process.env.PORT || 5000
